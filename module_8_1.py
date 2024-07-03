8e3de49dc52619e94d6629c306a0f9001dedac85
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-07-03 11:55:47 +0300

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -2,7 +2,7 @@ def add_everything_up(a, b):
     try:
         return round((a + b), 3)
     except TypeError:
-        return f'{str(a)} {str(b)}'
+        return f'{str(a) + str(b)}'
 
 
 print(add_everything_up(123.456, 'строка'))
